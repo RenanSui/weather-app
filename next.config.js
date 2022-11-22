e6bfe8faea250fc57cb02983d262b042d511d266
renansui <renanddtao@gmail.com>
2022-11-22 03:50:49 -0300

    added headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
+const { get } = require('http');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports.NextConfig = nextConfig;
+
+module.exports = {
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,POST,PUT,DELETE,HEAD,OPTIONS',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'Content-Type,X-Requested-With,Accept,Authorization,Origin,Access-Control-Request-Method,Access-Control-Request-Headers',
+                    },
+                ],
+            },
+        ];
+    },
+};
